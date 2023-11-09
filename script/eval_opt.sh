6a6f76783e5f892c701d9736b85aacb100919a3c
kibnapper <1259478761@qq.com>
2023-11-09 07:05:06 -0500

    speed test 增加linear模块

--- script/eval_opt.sh
+++ script/eval_opt.sh
@@ -11,13 +11,15 @@
 #     --save weight/opt350m-3bit.pt
 
 
-CUDA_VISIBLE_DEVICES=0 python opt.py \
-    facebook/opt-350m \
-    wikitext2 \
-    --wbits 3 \
-    --load weight/opt350m-3bit.pt \
-    --benchmark 128 
+# CUDA_VISIBLE_DEVICES=0 python opt.py \
+#     facebook/opt-350m \
+#     wikitext2 \
+#     --wbits 3 \
+#     --load weight/opt350m-3bit.pt \
+#     --benchmark 128 
 
+
+# ====================================================================================================
 
 # CUDA_VISIBLE_DEVICES=0 python opt.py \
 #     facebook/opt-125m \
@@ -39,6 +41,8 @@ CUDA_VISIBLE_DEVICES=0 python opt.py \
 #     --benchmark 128 
 
 
+# ====================================================================================================
+
 # CUDA_VISIBLE_DEVICES=0 python opt.py \
 #     facebook/opt-1.3b \
 #     wikitext2 \
@@ -56,4 +60,31 @@ CUDA_VISIBLE_DEVICES=0 python opt.py \
 #     wikitext2 \
 #     --wbits 3 \
 #     --load weight/opt1.3b-3bit.pt \
+#     --benchmark 128 
+
+
+# ====================================================================================================
+
+CUDA_VISIBLE_DEVICES=0 python opt.py \
+    facebook/opt-13b \
+    wikitext2 \
+    --benchmark 128 
+
+# CUDA_VISIBLE_DEVICES=0,1,2,4,5 python opt.py \
+#     facebook/opt-30b \
+#     wikitext2 \
+#     --wbits 3 \
+#     --save weight/opt30b-3bit.pt
+
+# CUDA_VISIBLE_DEVICES=0,1,2,4,5 python opt.py \
+#     facebook/opt-13b \
+#     wikitext2 \
+#     --wbits 3 \
+#     --save weight/opt13b-3bit.pt
+
+# CUDA_VISIBLE_DEVICES=0,1,2,4,5 python opt.py \
+#     facebook/opt-30b \
+#     wikitext2 \
+#     --wbits 3 \
+#     --load weight/opt30b-3bit.pt \
 #     --benchmark 128 
